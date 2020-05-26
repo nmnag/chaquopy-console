b4aaa7b56793cc77f74080d834fe2951eaf1d181
naxhy <nmnag@iitk.ac.in>
2020-05-26 20:11:11 +0530

    Add Ons to Parse a pdf

--- app/src/main/java/com/chaquo/python/console/MainActivity.java
+++ app/src/main/java/com/chaquo/python/console/MainActivity.java
@@ -1,9 +1,57 @@
 package com.chaquo.python.console;
 
 import android.app.*;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
 import com.chaquo.python.utils.*;
 
 public class MainActivity extends PythonConsoleActivity {
+
+    String textView;
+    Button btn_fp;
+    Intent myfileintent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_console);
+        btn_fp= (Button) findViewById(R.id.btn_fp);
+
+        btn_fp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                myfileintent =new Intent(Intent.ACTION_GET_CONTENT);
+                myfileintent.setType("*/*");
+                startActivityForResult(myfileintent,10);
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK) {
+                    String path = data.getData().getPath();
+                    textView = path;
+
+                }
+                break;
+        }
+
+    }
 
     @Override protected Class<? extends Task> getTaskClass() {
         return Task.class;
@@ -15,7 +63,7 @@ public class MainActivity extends PythonConsoleActivity {
         }
 
         @Override public void run() {
-            py.getModule("main").callAttr("main");
+            py.getModule("main(textview)").callAttr("main(textview)");
         }
     }
 }
--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -1,13 +1,19 @@
 from six.moves import input
+import requests
+import pdfplumber
 
 
-def main():
+def main(string):
     print("Enter your name, or an empty line to exit.")
-    while True:
-        try:
-            name = input()
-        except EOFError:
-            break
-        if not name:
-            break
-        print("Hello {}!".format(name))
+    with pdfplumber.load(string) as pdf:
+        first_page = pdf.pages[0]
+        text = first_page.extract_text()
+        print(text)
+    # while True:
+    #     try:
+    #         name = input()
+    #     except EOFError:
+    #         break
+    #     if not name:
+    #         break
+    #     print("Hello {}!".format(name))
